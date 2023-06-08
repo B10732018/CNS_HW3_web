e128a5f592a658d7cfe0470e2f2b60a3f74c0d6b
B10732018 <71759401+B10732018@users.noreply.github.com>
2023-06-08 16:15:10 +0800

    Update code3a.py

--- code3a.py
+++ code3a.py
@@ -6,16 +6,42 @@ import time
 port = int(os.getenv('PORT', 3000))
 hostName = '0.0.0.0'
 
+username='CNS-user'
+password='CNS-password'
+
 class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
+    def basic_auth(self, username, password):
+        token = b64encode(f"{username}:{password}".encode('utf-8')).decode("ascii")
+        return token
+    def do_AUTHHEAD(self):
+        self.send_response(401)
+        self.send_header("WWW-Authenticate", 'Basic realm="Test"')
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+    def do_GET(self):
+        if self.headers.get("Authorization") == None:
+            self.do_AUTHHEAD()
+            self.wfile.write(bytes("failed", 'utf-8'))
+        elif self.headers.get("Authorization") == "Basic " + self.basic_auth(username, password):
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes("ok", 'utf-8'))
+        else:
+            self.do_AUTHHEAD()
+            self.wfile.write(bytes("failed", 'utf-8'))
+    def do_POST(self):
+        if self.headers.get("Authorization") == None:
+            self.do_AUTHHEAD()
+            self.wfile.write(bytes("failed", 'utf-8'))
+        elif self.headers.get("Authorization") == "Basic " + self.basic_auth(username, password):
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes("ok", 'utf-8'))
+        else:
+            self.do_AUTHHEAD()
+            self.wfile.write(bytes("failed", 'utf-8'))
 
 if __name__ == "__main__":
     webServer = HTTPServer((hostName, port), MyServer)
